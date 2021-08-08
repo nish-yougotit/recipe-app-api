d9b7b6819754f528e7e87ac08df533b7a3a5a178
nish-yougotit <shivamgupta26ng@gmail.com>
2021-08-08 08:08:48 +0530

    added flake8 for testing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,16 @@ class TestModels(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        email = 'test@londonappdevloper@GMAIL.COM'
+        password = 'test12234'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+            )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
